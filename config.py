0efbf6899325c2303bc4dcde79c167f8018fa58d
creativedragon24 <rajshashikantpawaskar@ug.edu.ge>
2026-06-21 21:49:30 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# This tells the scraper what browser to pretend to be
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+DATA_DIR = "."
